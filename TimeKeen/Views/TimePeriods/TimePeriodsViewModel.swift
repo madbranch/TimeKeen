b8eac055f84be4b4a142c8559be59d067e8828f6
Adam Labranche <adam@labranche.co>
2024-07-14 07:38:48 -0400

    Fetch time entries

--- TimeKeen/Views/TimePeriods/TimePeriodsViewModel.swift
+++ TimeKeen/Views/TimePeriods/TimePeriodsViewModel.swift
@@ -1,16 +1,18 @@
 import Foundation
+import SwiftUI
 import CoreData
 
 final class TimePeriodsViewModel: ObservableObject {
   private var context: NSManagedObjectContext
+  @FetchRequest(fetchRequest: requestEntries) private var entries: FetchedResults<TimeEntry>
 
   init(context: NSManagedObjectContext) throws {
     self.context = context
-    
-    let fetchRequest: NSFetchRequest<TimeEntry>
-    fetchRequest = TimeEntry.fetchRequest()
-    fetchRequest.sortDescriptors = [NSSortDescriptor(key: "start", ascending: false)]
-    
-    //let entries = try context.fetch(fetchRequest)
   }
+
+  static let requestEntries: NSFetchRequest = {
+    let request = TimeEntry.fetchRequest()
+    request.sortDescriptors = [NSSortDescriptor(key: "start", ascending: false)]
+    return request
+  }()
 }
