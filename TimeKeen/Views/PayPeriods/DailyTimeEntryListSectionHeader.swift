145549fa22344e5cc36b430ee7a4c0ae6933f3e8
Adam Labranche <adam@labranche.co>
2024-07-19 07:29:10 -0400

    Move DailyTimeEntryListSectionHeader to its own file

---
+++ TimeKeen/Views/PayPeriods/DailyTimeEntryListSectionHeader.swift
@@ -0,0 +1,18 @@
+import SwiftUI
+
+struct DailyTimeEntryListSectionHeader: View {
+  @ObservedObject var viewModel: TimeEntryListViewModel
+  private static let durationStyle = Duration.TimeFormatStyle(pattern: .hourMinute)
+
+  init(viewModel: TimeEntryListViewModel) {
+    self.viewModel = viewModel
+  }
+
+  var body: some View {
+    HStack {
+      Text(viewModel.timeEntries[0].start.formatted(date: .complete, time: .omitted))
+      Spacer()
+      Text(viewModel.duration.formatted(DailyTimeEntryListSectionHeader.durationStyle))
+    }
+  }
+}
