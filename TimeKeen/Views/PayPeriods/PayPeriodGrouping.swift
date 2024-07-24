ccfdd7124706b31395c4e8ea76edbf824981fb30
Adam Labranche <adam@labranche.co>
2024-07-23 22:06:30 -0400

    Implement pay period grouping by monthly and 4 weeks

--- TimeKeen/Views/PayPeriods/PayPeriodGrouping.swift
+++ TimeKeen/Views/PayPeriods/PayPeriodGrouping.swift
@@ -6,13 +6,47 @@ extension Array where Element == TimeEntry {
   }
 }
 
+extension Calendar {
+  func previousDay(from date: Date) -> Date? {
+    return self.date(byAdding: .day, value: -1, to: date)
+  }
+  
+  func nextDay(from date: Date) -> Date? {
+    return self.date(byAdding: .day, value: 1, to: date)
+  }
+  
+  func previousMonth(from date: Date) -> Date? {
+    return self.date(byAdding: .month, value: -1, to: date)
+  }
+  
+  func nextMonth(from date: Date) -> Date? {
+    return self.date(byAdding: .month, value: 1, to: date)
+  }
+  
+  func startMonth(from date: Date) -> Date? {
+    return self.date(from: self.dateComponents([.year, .month], from: date))
+  }
+  
+  func endMonth(from date: Date) -> Date? {
+    return self.previousDay(from: self.startMonth(from: self.nextMonth(from: date)!)!)!
+  }
+  
+  func periodStart(ending periodEnd: Date) -> Date? {
+    return self.nextDay(from: self.previousMonth(from: periodEnd)!)
+  }
+  
+  func periodEnd(starting periodStart: Date) -> Date? {
+    return self.nextMonth(from: self.previousDay(from: periodStart)!)
+  }
+}
+
 struct PayPeriodGrouping {
   static func getGroupByMethod(schedule: PayPeriodSchedule, periodEnd: Date) -> (TimeEntry) -> ClosedRange<Date> {
     return switch schedule {
     case .Weekly: getGroupByWeekly(periodEnd: periodEnd)
-    case .Biweekly: getGroupByBiweekly(periodEnd: periodEnd)
+    case .Biweekly: getGroupByNWeekly(periodEnd: periodEnd, nbWeeks: 2)
     case .Monthly: getGroupByMonthly(periodEnd: periodEnd)
-    case .EveryFourWeeks: getGroupByEveryFourWeeks(periodEnd: periodEnd)
+    case .EveryFourWeeks: getGroupByNWeekly(periodEnd: periodEnd, nbWeeks: 4)
     case .FirstAndSixteenth: getGroupByFirstAndSixteenth(periodEnd: periodEnd)
     }
   }
@@ -29,32 +63,40 @@ struct PayPeriodGrouping {
     }
   }
   
-  private static func getGroupByBiweekly(periodEnd: Date) -> (TimeEntry) -> ClosedRange<Date> {
+  private static func getGroupByNWeekly(periodEnd: Date, nbWeeks: Int) -> (TimeEntry) -> ClosedRange<Date> {
     let calendar = Calendar.current
-    let biweekdays = calendar.weekdaySymbols.count * 2
+    let nbDays = calendar.weekdaySymbols.count * nbWeeks
     let periodStart = calendar.date(byAdding: .day, value: 1, to: periodEnd)!
     return {
       let start = $0.start
       let deltaComponents = calendar.dateComponents([.day], from: periodStart, to: start)
       let deltaDays = deltaComponents.day!
-      let delta = (deltaDays / biweekdays) * biweekdays
+      let delta = (deltaDays / nbDays) * nbDays
       let currentPeriodStart = calendar.date(byAdding: .day, value: delta, to: periodEnd)!
-      let currentPeriodEnd = calendar.date(byAdding: .day, value: biweekdays - 1, to: currentPeriodStart)!
+      let currentPeriodEnd = calendar.date(byAdding: .day, value: nbDays - 1, to: currentPeriodStart)!
       return currentPeriodStart...currentPeriodEnd
     }
   }
   
   private static func getGroupByMonthly(periodEnd: Date) -> (TimeEntry) -> ClosedRange<Date> {
-    //let calendar = Calendar.current
-    //let periodStart = calendar.date(byAdding: .day, value: 1, to: calendar.date(byAdding: .month, value: -1, to: periodEnd)!)!
+    let calendar = Calendar.current
+    let periodEndComponents = calendar.dateComponents([.year, .month, .day], from: periodEnd)
     return {
       let start = $0.start
-      return start...$0.end
+      let startComponents = calendar.dateComponents([.year, .month, .day], from: start)
+      let nbDaysInStartMonth = calendar.range(of: .day, in: .month, for: start)!.count
+      let startMonthEndPeriodDay = min(nbDaysInStartMonth, periodEndComponents.day!)
+      
+      if startComponents.day! <= startMonthEndPeriodDay {
+        let currentPeriodEnd = calendar.date(from: DateComponents(year: startComponents.year, month: startComponents.month, day: startMonthEndPeriodDay))!
+        let currentPeriodStart = calendar.periodStart(ending: currentPeriodEnd)!
+        return currentPeriodStart...currentPeriodEnd
+      }
+
+      let currentPeriodStart = calendar.nextDay(from: calendar.date(from: DateComponents(year: startComponents.year, month: startComponents.month, day: startMonthEndPeriodDay))!)!
+      let currentPeriodEnd = calendar.periodEnd(starting: currentPeriodStart)!
+      return currentPeriodStart...currentPeriodEnd
     }
-  }
-  
-  private static func getGroupByEveryFourWeeks(periodEnd: Date) -> (TimeEntry) -> ClosedRange<Date> {
-    return getGroupByWeekly(periodEnd: periodEnd)
   }
   
   private static func getGroupByFirstAndSixteenth(periodEnd: Date) -> (TimeEntry) -> ClosedRange<Date> {
