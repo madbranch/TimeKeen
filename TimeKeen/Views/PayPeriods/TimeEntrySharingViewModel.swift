5096bb290d727994848984b9e8c9fd8860f220c8
Adam Labranche <adam@labranche.co>
2024-07-28 06:08:55 -0400

    Export data to CSV

--- TimeKeen/Views/PayPeriods/TimeEntrySharingViewModel.swift
+++ TimeKeen/Views/PayPeriods/TimeEntrySharingViewModel.swift
@@ -38,19 +38,26 @@ struct TimeEntryJsonExport: Transferable {
   
   func commaSeparatedText() -> Data {
     do {
-      let dateRange = from...to
-      let descriptor = FetchDescriptor<TimeEntry>(predicate: #Predicate<TimeEntry> { dateRange.contains($0.start) }, sortBy: [SortDescriptor(\.start, order: .reverse)])
+      let calendar = Calendar.current
+      let from = calendar.startOfDay(for: self.from)
+      let to = calendar.startOfDay(for: calendar.nextDay(from: self.to)!)
+      let descriptor = FetchDescriptor<TimeEntry>(predicate: #Predicate<TimeEntry> { $0.start >= from && $0.start < to }, sortBy: [SortDescriptor(\.start, order: .reverse)])
       let timeEntries = try context.fetch(descriptor)
       
       var text = String(localized: "From,To,Number of Breaks,On Break,On the Clock,Notes\n")
       
-      let formatter = ISO8601DateFormatter()
+      let dateFormatter = ISO8601DateFormatter()
       
       for timeEntry in timeEntries {
-        text.append("\(formatter.string(from: timeEntry.start)),\(formatter.string(from: timeEntry.end)),\"\(timeEntry.notes)\"\n")
+        let from = dateFormatter.string(from: timeEntry.start)
+        let to = dateFormatter.string(from: timeEntry.end)
+        let numberOfBreaks = timeEntry.breaks.count
+        let onBreak = Formatting.timeIntervalFormatter.string(from: timeEntry.onBreak) ?? ""
+        let onTheClock = Formatting.timeIntervalFormatter.string(from: timeEntry.onTheClock) ?? ""
+        text.append("\(from),\(to),\(numberOfBreaks),\(onBreak),\(onTheClock),\(timeEntry.notes)\n")
       }
       
-      return Data()
+      return Data(text.utf8)
     } catch {
       return Data()
     }
