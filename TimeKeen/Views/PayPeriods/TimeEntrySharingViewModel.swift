16322978ff8b9e52cc0fd6728d2a5221d3507452
Adam Labranche <adam@labranche.co>
2024-07-28 06:13:01 -0400

    Extract TimeEntry fetching into its own method

--- TimeKeen/Views/PayPeriods/TimeEntrySharingViewModel.swift
+++ TimeKeen/Views/PayPeriods/TimeEntrySharingViewModel.swift
@@ -36,31 +36,33 @@ struct TimeEntryJsonExport: Transferable {
     jsonExport = TimeEntryJsonExport(viewModel: self)
   }
   
-  func commaSeparatedText() -> Data {
+  private func fetchEntries() -> [TimeEntry] {
     do {
       let calendar = Calendar.current
       let from = calendar.startOfDay(for: self.from)
       let to = calendar.startOfDay(for: calendar.nextDay(from: self.to)!)
       let descriptor = FetchDescriptor<TimeEntry>(predicate: #Predicate<TimeEntry> { $0.start >= from && $0.start < to }, sortBy: [SortDescriptor(\.start, order: .reverse)])
-      let timeEntries = try context.fetch(descriptor)
-      
-      var text = String(localized: "From,To,Number of Breaks,On Break,On the Clock,Notes\n")
-      
-      let dateFormatter = ISO8601DateFormatter()
-      
-      for timeEntry in timeEntries {
-        let from = dateFormatter.string(from: timeEntry.start)
-        let to = dateFormatter.string(from: timeEntry.end)
-        let numberOfBreaks = timeEntry.breaks.count
-        let onBreak = Formatting.timeIntervalFormatter.string(from: timeEntry.onBreak) ?? ""
-        let onTheClock = Formatting.timeIntervalFormatter.string(from: timeEntry.onTheClock) ?? ""
-        text.append("\(from),\(to),\(numberOfBreaks),\(onBreak),\(onTheClock),\(timeEntry.notes)\n")
-      }
-      
-      return Data(text.utf8)
+      return try context.fetch(descriptor)
     } catch {
-      return Data()
+      return [TimeEntry]()
     }
+  }
+  
+  func commaSeparatedText() -> Data {
+    let timeEntries = fetchEntries()
+    var text = String(localized: "From,To,Number of Breaks,On Break,On the Clock,Notes\n")
+    let dateFormatter = ISO8601DateFormatter()
+    
+    for timeEntry in timeEntries {
+      let from = dateFormatter.string(from: timeEntry.start)
+      let to = dateFormatter.string(from: timeEntry.end)
+      let numberOfBreaks = timeEntry.breaks.count
+      let onBreak = Formatting.timeIntervalFormatter.string(from: timeEntry.onBreak) ?? ""
+      let onTheClock = Formatting.timeIntervalFormatter.string(from: timeEntry.onTheClock) ?? ""
+      text.append("\(from),\(to),\(numberOfBreaks),\(onBreak),\(onTheClock),\(timeEntry.notes)\n")
+    }
+    
+    return Data(text.utf8)
   }
   
   func json() -> Data {
