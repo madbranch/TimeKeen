a352ae5c176fc98bcdb946dd47b9b260a0fd7cdc
Adam Labranche <adam@labranche.co>
2024-08-17 17:31:16 -0400

    Create PayPeriodRow view

--- TimeKeen/Views/PayPeriods/PayPeriodList.swift
+++ TimeKeen/Views/PayPeriods/PayPeriodList.swift
@@ -13,18 +13,7 @@ struct PayPeriodList: View {
   var body: some View {
     List(allTimeEntries.group(by: payPeriodSchedule, ending: endOfLastPayPeriod)) { payPeriod in
       NavigationLink(value: payPeriod) {
-        HStack {
-          VStack(alignment: .leading) {
-            Text("\(Formatting.yearlessDateformatter.string(from: payPeriod.range.lowerBound)) - \(Formatting.yearlessDateformatter.string(from: payPeriod.range.upperBound))")
-              .font(.headline)
-            Text(payPeriod.timeEntries.count == 1 ? "1 entry" : "\(payPeriod.timeEntries.count) entries")
-              .font(.caption)
-              .foregroundStyle(.secondary)
-          }
-          Spacer()
-          Text(Formatting.timeIntervalFormatter.string(from: payPeriod.timeEntries.reduce(TimeInterval()) { $0 + $1.onTheClock }) ?? "")
-            .foregroundStyle(.secondary)
-        }
+        PayPeriodRow(payPeriod: payPeriod)
       }
     }
     .navigationDestination(for: PayPeriod.self) { payPeriod in
