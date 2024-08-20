422d52296e458a63ba7e32c858066aa6f8334fb3
Adam Labranche <adam@labranche.co>
2024-08-20 00:02:38 -0400

    Move TimeClockButton to its own file

--- TimeKeen/Views/CurrentTimeEntry/CurrentTimeEntryView.swift
+++ TimeKeen/Views/CurrentTimeEntry/CurrentTimeEntryView.swift
@@ -1,21 +1,5 @@
 import SwiftUI
 
-struct TimeClockButton: ButtonStyle {
-  @Environment(\.isEnabled) var isEnabled
-  func makeBody(configuration: Configuration) -> some View {
-    configuration.label
-      .background(
-        Circle()
-          .fill(LinearGradient(gradient: Gradient(colors: [Color(red: 31/255, green: 126/255, blue: 161/255), Color(red: 111/255, green: 247/255, blue: 232/255)]), startPoint: .top, endPoint: .bottom))
-      )
-      .foregroundColor(.white)
-      .contentShape(.circle)
-      .shadow(radius: 10)
-      .opacity(configuration.isPressed ? 0.5 : 1)
-      .saturation(isEnabled ? 1 : 0)
-  }
-}
-
 struct CurrentTimeEntryView: View {
   var quickActionProvider: QuickActionProvider
   
