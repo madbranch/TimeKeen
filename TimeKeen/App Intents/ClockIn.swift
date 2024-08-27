7ad00d8cba9e6d6fd65eab6d5f77336000bc41db
Adam Labranche <adam@labranche.co>
2024-08-26 23:37:54 -0400

    Add parameter to ClockIn intent

--- TimeKeen/App Intents/ClockIn.swift
+++ TimeKeen/App Intents/ClockIn.swift
@@ -5,24 +5,26 @@ struct ClockIn: AppIntent {
   static var title: LocalizedStringResource = "Clock In"
   static var description = IntentDescription("Clock in and start working.")
   
+  @Parameter(title: "When", description: "When to clock in.")
+  var when: Date
+  
   @MainActor
-  func perform() async throws -> some IntentResult {
+  func perform() async throws -> some IntentResult & ProvidesDialog {
     guard let userDefaults = SharedData.userDefaults else {
-      return .result()
+      return .result(dialog: "Failed to clock in.")
     }
     
     guard userDefaults.clockInState == .clockedOut else {
-      return .result()
+      return .result(dialog: "You're already clocked in.")
     }
     
-    let minuteInterval = userDefaults.minuteInterval
-    let clockInDate = Calendar.current.getRoundedDate(minuteInterval: minuteInterval, from: Date())
+    let clockInDate = Calendar.current.getRoundedDate(minuteInterval: userDefaults.minuteInterval, from: when)
     
     userDefaults.notes = ""
     userDefaults.breaks = [BreakEntry]()
     userDefaults.clockInDate = clockInDate
     userDefaults.clockInState = .clockedInWorking
     
-    return .result()
+    return .result(dialog: "Clocking in at \(Formatting.startEndFormatter.string(from: clockInDate))")
   }
 }
--- TimeKeen/App Intents/TimeKeenShortcuts.swift
+++ TimeKeen/App Intents/TimeKeenShortcuts.swift
@@ -4,7 +4,7 @@ import AppIntents
 class TimeKeenShortcuts: AppShortcutsProvider {
   static var appShortcuts: [AppShortcut] {
     AppShortcut(intent: ClockIn(), phrases: [
-      "Clock In",
+      "Clock in",
       "Start working"
     ],
     shortTitle: "Clock In",
