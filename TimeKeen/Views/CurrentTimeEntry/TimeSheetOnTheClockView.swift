d9b47c5435bf5f7c1eacbb9bd5d002aa706dbd70
Adam Labranche <adam@labranche.co>
2024-09-06 17:40:01 -0400

    Tweaked TimeSheetOnTheClockView

--- TimeKeen/Views/CurrentTimeEntry/TimeSheetOnTheClockView.swift
+++ TimeKeen/Views/CurrentTimeEntry/TimeSheetOnTheClockView.swift
@@ -21,7 +21,7 @@ struct TimeSheetOnTheClockView: View {
         if t > 0 {
             Label("Worked **\(Formatting.timeIntervalFormatter.string(from: t) ?? "")** since \(Formatting.yearlessDateformatter.string(from: payPeriod.lowerBound))", systemImage: "stopwatch")
                 .contentTransition(.numericText(value: clockInDuration))
-                .frame(maxWidth: .infinity)
+                .frame(maxWidth: .infinity, alignment: .leading)
                 .padding()
                 .background(
                     RoundedRectangle(cornerRadius: 12)
