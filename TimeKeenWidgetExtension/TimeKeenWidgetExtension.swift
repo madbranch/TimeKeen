72c1eb985cf77dc85de5f408f5e53ed1088a875d
Adam Labranche <adam@labranche.co>
2024-12-24 07:31:56 -0500

    Show total time worked during pay period in widget

--- TimeKeenWidgetExtension/TimeKeenWidgetExtension.swift
+++ TimeKeenWidgetExtension/TimeKeenWidgetExtension.swift
@@ -1,5 +1,6 @@
 import WidgetKit
 import SwiftUI
+import SwiftData
 
 struct Provider: TimelineProvider {
     func placeholder(in context: Context) -> SimpleEntry {
@@ -12,11 +13,13 @@ struct Provider: TimelineProvider {
     
     func getTimeline(in context: Context, completion: @escaping @Sendable (Timeline<SimpleEntry>) -> Void) {
         let entry = Self.getUpdatedEntry()
-        completion(Timeline(entries: [entry], policy: .never))
+        // We specify to request a new timeline at the end of the pay period to make sure the current pay period is always up-to-date.
+        completion(Timeline(entries: [entry], policy: .after(entry.payPeriod.upperBound)))
     }
     
     static func getEmptyEntry() -> SimpleEntry {
-        SimpleEntry(date: .now, clockInState: .clockedOut, clockInDate: .now, breakStart: .now, onBreak: TimeInterval())
+        let emptyPayPeriod = Date.now.getPayPeriod(schedule: .Weekly, periodEnd: Calendar.current.date(from: DateComponents(year: 2024, month: 07, day: 21))!)
+        return SimpleEntry(date: .now, clockInState: .clockedOut, clockInDate: .now, breakStart: .now, onBreak: TimeInterval(), payPeriod: emptyPayPeriod)
     }
     
     static func getUpdatedEntry() -> SimpleEntry {
@@ -33,7 +36,10 @@ struct Provider: TimelineProvider {
         let breakStart = userDefaults.breakStart ?? .now
         let breaks = userDefaults.breaks ?? []
         let onBreak = breaks.reduce(TimeInterval()) { $0 + $1.interval }
-        let entry = SimpleEntry(date: .now, clockInState: clockInState, clockInDate: clockInDate, breakStart: breakStart, onBreak: onBreak)
+        let payPeriodSchedule = userDefaults.payPeriodSchedule
+        let endOfLastPayPeriod = userDefaults.endOfLastPayPeriod
+        let payPeriod = Date.now.getPayPeriod(schedule: payPeriodSchedule, periodEnd: endOfLastPayPeriod)
+        let entry = SimpleEntry(date: .now, clockInState: clockInState, clockInDate: clockInDate, breakStart: breakStart, onBreak: onBreak, payPeriod: payPeriod)
         return entry
     }
 }
@@ -44,11 +50,20 @@ struct SimpleEntry: TimelineEntry {
     let clockInDate: Date
     let breakStart: Date
     let onBreak: TimeInterval
+    let payPeriod: ClosedRange<Date>
 }
 
 struct TimeKeenWidgetExtensionEntryView : View {
     var entry: Provider.Entry
-    
+    @Query var timeEntries: [TimeEntry]
+
+    init(entry: Provider.Entry) {
+        self.entry = entry
+        _timeEntries = Query(filter: #Predicate<TimeEntry> { [payPeriod = self.entry.payPeriod] timeEntry in
+            return timeEntry.start >= payPeriod.lowerBound && timeEntry.start <= payPeriod.upperBound
+        })
+    }
+
     @Environment(\.widgetFamily) var family
 
     var body: some View {
@@ -72,12 +87,18 @@ struct TimeKeenWidgetExtensionEntryView : View {
                     
                     if timerDate > .now {
                         Text("--")
-                    }
-                    else {
+                    } else {
                         Text("\(timerDate, style: .timer)")
                             .multilineTextAlignment(.center)
                             .font(.headline)
                     }
+                    
+                    let payPeriodOnTheClock: TimeInterval = timeEntries.reduce(TimeInterval()) { $0 + $1.onTheClock }
+                    let payPeriodTimerDate = entry.clockInDate + entry.onBreak - payPeriodOnTheClock
+                    
+                    Text( "Total \(payPeriodTimerDate, style: .timer)")
+                        .multilineTextAlignment(.center)
+                        .font(.caption)
                 }
             case .clockedInTakingABreak:
                 Text("On Break")
@@ -94,6 +115,15 @@ struct TimeKeenWidgetExtensionEntryView : View {
     }
 }
 
+// globals are lazy
+fileprivate let modelContainer: ModelContainer = {
+    do {
+        return try ModelContainer(for: TimeEntry.self, BreakEntry.self, configurations: ModelConfiguration(isStoredInMemoryOnly: false))
+    } catch {
+        fatalError("Failed to configure SwiftData container.")
+    }
+}()
+
 struct TimeKeenWidgetExtension: Widget {
     let kind: String = "TimeKeenWidgetExtension"
     
@@ -101,6 +131,7 @@ struct TimeKeenWidgetExtension: Widget {
         StaticConfiguration(kind: kind, provider: Provider()) { entry in
             TimeKeenWidgetExtensionEntryView(entry: entry)
                 .containerBackground(.fill.tertiary, for: .widget)
+                .modelContainer(modelContainer)
         }
         #if os(watchOS)
         .supportedFamilies([.accessoryCircular, .accessoryRectangular, .accessoryInline])
